c6664c7ff6fdea3c2a3b69429bb227cb7ad3bfe5
vivekpal5682 <138307385+vivekpal5682@users.noreply.github.com>
2024-02-24 17:48:06 +0530

    upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function show() {
+ var blur = document.getElementById('mainForm');
+ blur.style.display="block";
+}
+function cancel() {
+ var blur = document.getElementById('mainForm');
+ blur.style.display="none";
+}
+
+var right=document.getElementById("navLinks")
+function showmenu(){
+    navLinks.style.display= "block"
+}
+function hidemenu(){
+    navLinks.style.display= "none"
+}
